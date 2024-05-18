8a8c854d3ddc01a2cb8ce1a3e6b1ec436951ac65
Jakub Mladý <scrub@students.zcu.cz>
2024-05-18 11:06:47 +0200

    CLI

--- ECC.py
+++ ECC.py
@@ -1,4 +1,5 @@
 import secrets
+from base64 import b64encode, b64decode
 
 class ECPoint:
     """
@@ -9,6 +10,8 @@ class ECPoint:
         self.x = x
         self.y = y
         self.space = space
+        self._ord = None
+        self._nbits = space.p.bit_length()
 
     def __add__(self, other):
         if self.x == other.x and self.y == other.y:
@@ -40,13 +43,29 @@ class ECPoint:
                         (self.space.p-self.y) % self.space.p, 
                         self.space)
 
+    @property
     def order(self):
+        if self._ord is not None:
+            return self._ord
         p = self
         n = 1
         while p.x is not None:
             p = p + self
             n += 1
+        self._ord = n
         return n
+    
+    def serialize(self):
+        return b64encode(  self.x.to_bytes((self._nbits+7)//8, 'big') \
+                         + self.y.to_bytes((self._nbits+7)//8, 'big')).decode()
+    
+    @staticmethod
+    def deserialize(b64str, space):
+        b = b64decode(b64str)
+        x = int.from_bytes(b[:len(b)//2], 'big')
+        y = int.from_bytes(b[len(b)//2:], 'big')
+        return ECPoint(x, y, space)
+
 
 class ECSpace:
     """
@@ -72,7 +91,7 @@ class ECKey:
     Elliptic Curve Key Pair
     """
 
-    def __init__(self, space, generator, pub_key=None):
+    def __init__(self, space, generator, pub_key=None, priv_key=None):
         if space.is_singular():
             raise ValueError("Singular curve")
         if not space.is_valid(generator):
@@ -80,50 +99,102 @@ class ECKey:
         
         self.space = space
         self.generator = generator
-        self.order = generator.order()
-        self.priv_key = None
+        self.order = generator.order
+        self.priv_key = priv_key
         self.pub_key = pub_key
 
     def generate(self):
         if self.priv_key is not None:
-            return
+            self.pub_key = self.generator * self.priv_key
+            return self
         self.pub_key = ECPoint(None, None, self.space)
         while self.pub_key.x is None:
             self.priv_key = secrets.randbelow(self.order)
             self.pub_key = self.generator * self.priv_key
         return self
 
+space_codes = {
+    "secp256k1": (ECSpace(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
+                          0x0, 0x7),
+                  (0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+                   0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8),
+                   0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141)
+}
 
+def load_space(space):
+    if space not in space_codes:
+        raise ValueError("Unknown space")
+    sp = space_codes[space][0]
+    generator = ECPoint(*space_codes[space][1], sp)
+    generator._ord = space_codes[space][2]
+    return sp, generator
 
-def ecdsa_sign(message, key):
+class ECDSA:
+
+    def __init__(self, key):
+        self.key = key
+
+    def sign(self, message):
+        r,s=0,0
+        while True:
+            rnd = secrets.randbelow(self.key.order)
+
+            r = (self.key.generator * rnd).x 
+            if not r: continue # r cannot be None
+            r = r % self.key.order
+            if not r: continue # r cannot be 0
+            s = (pow(rnd,-1,self.key.order) * (message + self.key.priv_self.key * r)) % self.key.order
+            if not s: continue # s cannot be 0
+            break
+        return (r, s)
+
+    def verify(self, message, signature):
+        if not self.key.space.is_valid(self.key.pub_self.key):
+            return False
+        r, s = signature
+        if r < 1 or r > self.key.order - 1 or s < 1 or s > self.key.order - 1:
+            return False
+        w = pow(s,-1,self.key.order)
+        u1 = message * w % self.key.order
+        u2 = r * w % self.key.order
+        p = self.key.generator * u1 + self.key.pub_self.key * u2
+        return r == p.x % self.key.order
+
+class ECDH:
     """
-    ECDSA Sign
+    Elliptic Curve Diffie-Hellman
     """
 
-    r,s=0,0
-    while True:
-        rnd = secrets.randbelow(key.order)
+    def __init__(self, space, generator):
+        self.space = space
+        self.generator = generator
+        self.key = None
 
-        r = (key.generator * rnd).x 
-        if not r: continue # r cannot be None
-        r = r % key.order
-        if not r: continue # r cannot be 0
-        s = (pow(rnd,-1,key.order) * (message + key.priv_key * r)) % key.order
-        if not s: continue # s cannot be 0
-        break
-    return (r, s)
+    def generate(self):
+        self.key = ECKey(self.space, self.generator).generate()
+        return self.key
+    
+    def shared_secret(self, key):
+        return key.pub_key * self.key.priv_key
 
-def ecdsa_verify(message, signature, key):
-    """
-    ECDSA Verify
-    """
-    if not key.space.is_valid(key.pub_key):
-        return False
-    r, s = signature
-    if r < 1 or r > key.order - 1 or s < 1 or s > key.order - 1:
-        return False
-    w = pow(s,-1,key.order)
-    u1 = message * w % key.order
-    u2 = r * w % key.order
-    p = key.generator * u1 + key.pub_key * u2
-    return r == p.x % key.order
+if __name__ == "__main__":
+    p = 37
+    a = 2
+    b = 7
+    space = ECSpace(p, a, b)
+    x, G = 3, None
+    print("Finding a generator...")
+    for y in range(1, p):
+        if space.is_valid(ECPoint(x, y, space)):
+            print("Generator found!")
+            G = ECPoint(x,y, space)
+            break
+
+    # ===ALICE===
+    AKey = ECKey(space, G).generate()
+    message = 12
+    signature = ECDSA(AKey).sign(message)
+
+    # ===BOB===
+    Akey = ECKey(space, G, pub_key=AKey.pub_key)
+    ECDSA(Akey).verify(message, signature)
---
+++ ecc_cli.py
@@ -0,0 +1,187 @@
+import ECC
+from cmd2 import Cmd
+import sys
+
+if '--ecc-py-only' not in sys.argv:
+    try:
+        from Crypto.Cipher import AES
+        from base64 import b64encode, b64decode
+    except ImportError:
+        print("PyCryptodome could not be loaded. Some encryption functions will not work.")
+        AES = None
+else:
+    AES = None
+    print("Using ECC.py only. Some encryption functions will not work.")
+
+class ECCCLI(Cmd):
+
+    def __init__(self, space=None, gen=None, key=None):
+        super().__init__()
+        self.prompt = "ecc> "
+        self.intro = "Welcome to the ECC CLI. Type help or ? to list commands."
+
+        self.space = space
+        self.gen = gen
+        self.key = key
+
+    def do_set_space(self, arg):
+        """
+        Set or get the elliptic curve space.
+        Usage: `set_space p a b` or `set_space curve_name` or `set_space`
+        """
+        if not arg:
+            if self.space is None:
+                self.perror("No space specified.")
+                return
+            print("Space specification:")
+            print(f"p: {hex(self.space.p)}")
+            print(f"a: {hex(self.space.a)}")
+            print(f"b: {hex(self.space.b)}")
+            return
+
+        if arg.isdigit():
+            p, a, b = map(int, arg.split())
+            self.space = ECC.ECSpace(p, a, b)
+            if self.space.is_singular():
+                self.perror("Singular curve, abort operation.")
+                self.space = None
+            self.key = None
+        else:
+            self.space, self.gen = ECC.load_space(arg)
+            self.key = None
+
+    def do_set_generator(self, arg):
+        """
+        Set or get the generator point.
+        Usage: `set_generator x y` or `set_generator`
+        """
+        if not arg:
+            if self.gen is None:
+                self.perror("No generator point specified.")
+                return
+            print(f"Generator point: ({hex(self.gen.x)}, {hex(self.gen.y)})")
+            print(f"Order: {self.gen.order}")
+            return
+
+        if self.space is None:
+            self.perror("Space must be set first.")
+            return
+        x, y = map(int, arg.split())
+        self.gen = ECC.ECPoint(x, y, self.space)
+        if self.space.is_valid(self.gen):
+            self.perror("Invalid generator point, abort operation.")
+            self.gen = None
+            self.key = None
+
+    def ecdh_get_shared(self):
+        """
+        Perform an ECDH key exchange.
+        """
+        if self.space is None or self.gen is None:
+            raise ValueError("Space and generator must be set first.")
+        
+        ecdh = ECC.ECDH(self.space, self.gen)
+        key = ecdh.generate()
+        ser = key.pub_key.serialize()
+        print(f"Public key to send: {ser}")
+
+        ser = input("Enter the other party's public key: ")
+        key = ECC.ECKey.deserialize(ser, self.space)
+        shared = ecdh.shared_secret(key)
+        return shared
+    
+    def do_ecdh(self, arg):
+        """
+        Perform an ECDH key exchange.
+        Usage: `ecdh`
+        """
+        shared = self.ecdh_get_shared().serialize()
+        print(f"Shared secret: {shared}")
+
+    def do_ecies(self, arg):
+        """
+        Use AES encryption with prior ECDH key exchange.
+        Usage: `ecies`
+        """
+        if AES is None:
+            self.perror("PyCryptodome is not available.")
+            return
+
+        cli = ECCCLI_AES(self.ecdh_get_shared().serialize().encode())
+        cli.cmdloop()
+        
+def clear_console():
+    print("\033[H\033[J")
+
+class ECCCLI_AES(Cmd):
+
+    def __init__(self, key=None):
+        super().__init__()
+        self.prompt = "aes> "
+        self.intro = "Welcome to the AES CLI. Type help or ? to list commands."
+        self.register_postloop_hook(clear_console)
+
+        self.key = key
+        self.tag_length = 16
+        self.mode = "EAX"
+
+    def do_set_key(self, arg):
+        """
+        Set or get the AES key.
+        Usage: `set_key key` or `set_key`
+        """
+        if not arg:
+            if self.key is None:
+                self.perror("No key specified.")
+                return
+            print(f"Key: {self.key.hex()}")
+            return
+
+        self.key = arg.encode()
+
+    def do_set_mode(self, arg):
+        """
+        Get the AES mode.
+        Usage: `set_mode`
+        """
+        if not arg:
+            print(f"Mode: {self.mode}")
+            return
+        
+        self.perror("Mode setting not yet implemented.")
+
+    def do_encrypt(self, arg):
+        """
+        Encrypt a message.
+        Usage: `encrypt message`
+        """
+        if not arg:
+            self.perror("No message specified.")
+            return
+
+        cipher = AES.new(self.key, AES.MODE_EAX, mac_len=self.tag_length)
+        ciphertext, tag = cipher.encrypt_and_digest(arg.encode())
+        nonce = cipher.nonce
+        # 16 bytes nonce, 16 bytes tag, then the ciphertext
+        print("Encrypted message:")
+        print(b64encode(nonce + tag + ciphertext).decode())
+
+    def do_decrypt(self, arg):
+        """
+        Decrypt a message.
+        Usage: `decrypt message`
+        """
+        if not arg:
+            self.perror("No message specified.")
+            return
+
+        decoded = b64decode(arg)
+        nonce, tag, ciphertext = decoded[:16], decoded[16:32], decoded[32:]
+        cipher = AES.new(self.key, AES.MODE_EAX, mac_len=self.tag_length, nonce=nonce)
+        try:
+            plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+            print(f"Decrypted message: {plaintext.decode()}")
+        except ValueError:
+            self.perror("Decryption verification failed! The message may have been tampered with.")
+
+    
